1c8a57bada859b35e90fae60e36a9da6e227cfb8
Werdffelynir <werdffelynir@gmail.com>
2014-07-22 22:22:02 +0300

    start

--- main.php
+++ main.php
@@ -11,24 +11,24 @@ $xml->config(array(
 */
 
 /* @ready Возвращает список файлов в каталоге XML DB
-$listDocs = $xml->listDocs();
-*/
+ * $listDocs = $xml->listDocs();
+ */
 
 
 /* @ready
 $records = $xml->doc('articles')->item(3);
-$records = $xml->doc('articles')->item(3,'title');
-*/
+ * $records = $xml->doc('articles')->item(3,'title');
+ */
 
 
 /* @ready doc attr. Возвращает атрибут документа
-$records = $xml->doc('articles')->id; // return string '6'
-*/
+ * $records = $xml->doc('articles')->id; // return string '6'
+ */
 
 
 /* @ready doc items. Возвращает обект, первый елемент обекта под инексом [0]
-$records = $xml->doc('articles')->item; // return object(SimpleXMLElement)
-*/
+ * $records = $xml->doc('articles')->item; // return object(SimpleXMLElement)
+ */
 
 /* @ready
 $records = $xml->doc('articles')->attr(); // return array все атрибуты
@@ -39,15 +39,14 @@ $records = $xml->doc('articles')->attr('id'); // return string '6'
  */
 
 
-
 /* @ready
 $items = $xml->doc('articles')->item;
-//$items = $xml->doc('articles')->item();
-foreach($items as $item){
-    echo "<h1> <a href='$item[url]'>$item->title</a> </h1>";
-    echo "<div>$item->content</div>";
-    echo "<i>Опубликовано: ".date('m.d.Y H:i', ''+$item['date']).". Категория ($item[group]) </i>";
-}
+ * //$items = $xml->doc('articles')->item();
+ * foreach($items as $item){
+ * echo "<h1> <a href='$item[url]'>$item->title</a> </h1>";
+ * echo "<div>$item->content</div>";
+ * echo "<i>Опубликовано: ".date('m.d.Y H:i', ''+$item['date']).". Категория ($item[group]) </i>";
+ * }
  */
 
 
@@ -68,49 +67,105 @@ foreach ($articles->items() as $item) {
 */
 
 
-
-
-
 /* @ready toArray(TYPE=1) default
-$articles = $xml->doc('articles')->toArray();
-foreach ($articles as $item) {
-echo "<h2> <a href=\"$item[url]\">$item[title]</a> </h2>";
-echo "<div> $item[content] </div>";
-echo "<i>Опубликовано: ".date('m.d.Y H:i', $item['date']).". Категория ($item[group]) </i>";
-}
-*/
+ * $articles = $xml->doc('articles')->toArray();
+ * foreach ($articles as $item) {
+ * echo "<h2> <a href=\"$item[url]\">$item[title]</a> </h2>";
+ * echo "<div> $item[content] </div>";
+ * echo "<i>Опубликовано: ".date('m.d.Y H:i', $item['date']).". Категория ($item[group]) </i>";
+ * }
+ */
 
 
 /* @ready toArray(TYPE) TYPE 2
-$articles = $xml->doc('articles')->toArray(2);
-foreach ($articles as $item) {
-    echo "<h2> <a href=\"".$item['url']."\">".$item['item']['title']."</a> </h2>";
-    echo "<div> ".$item['item']['content']." </div>";
-    echo "<i>Опубликовано: ".date('m.d.Y H:i', $item['date']).". Категория (".$item['group'].") </i>";
-}
-*/
+ * $articles = $xml->doc('articles')->toArray(2);
+ * foreach ($articles as $item) {
+ * echo "<h2> <a href=\"".$item['url']."\">".$item['item']['title']."</a> </h2>";
+ * echo "<div> ".$item['item']['content']." </div>";
+ * echo "<i>Опубликовано: ".date('m.d.Y H:i', $item['date']).". Категория (".$item['group'].") </i>";
+ * }
+ */
 
 /* @ready toArray(TYPE) TYPE 3
-$articles = $xml->doc('articles')->toArray(3);
-foreach ($articles as $item) {
-echo "<h2> <a href=\"".$item['attr']['url']."\">$item[title]</a> </h2>";
-echo "<div> $item[content] </div>";
-echo "<hr/><i>Опубликовано: ".date('m.d.Y H:i', $item['attr']['date']).". Категория (".$item['attr']['group'].") </i>";
-}
+ * $articles = $xml->doc('articles')->toArray(3);
+ * foreach ($articles as $item) {
+ * echo "<h2> <a href=\"".$item['attr']['url']."\">$item[title]</a> </h2>";
+ * echo "<div> $item[content] </div>";
+ * echo "<hr/><i>Опубликовано: ".date('m.d.Y H:i', $item['attr']['date']).". Категория (".$item['attr']['group'].") </i>";
+ * }
+ */
+
+/* @no-ready toArray(TYPE) TYPE 4
+ * $item['attr-url']
+ * $item['a-url']
+ * $articles = $xml->doc('articles')->toArray(3);
+ * foreach ($articles as $item) {
+ * echo "<h2> <a href=\"".$item['attr-url']."\">$item[title]</a> </h2>";
+ * echo "<div> $item[content] </div>";
+ * echo "<hr/><i>Опубликовано: ".date('m.d.Y H:i', $item['attr-date']).". Категория (".$item['attr-group'].") </i>";
+ * }
+ *
+ * /* @no-ready toArray(TYPE) TYPE 5
+ * $articles = $xml->doc('articles')->toArray(3);
+ * foreach ($articles as $item) {
+ * echo "<h2> <a href=\"".$item['attr']['url']."\">$item['item'][title]</a> </h2>";
+ * echo "<div> $item['item'][content] </div>";
+ * echo "<hr/><i>Опубликовано: ".date('m.d.Y H:i', $item['attr']['date']).". Категория (".$item['attr']['group'].") </i>";
+ * }
+ */
+
+
+/* @ready create new document
+$xml->createDoc('newDoc2');
+$xml->addAttrs(array(
+        'group' => 'group',
+        'date' => date('m/d/Y H:i:s', time()-3600*24*30),
+    ));
+$xml->addItems(array(
+        'title' => 'title',
+        'charset' => 'charset',
+        'keywords' => 'keywords',
+        'description' => 'description',
+    ));
+$xml->addItemsAttrs(array(
+        'group' => '',
+        'date' => '',
+        'url' => '',
+        'order' => '',
+    ));
+$xml->save();
 */
 
 
+/** @ready  update records
+$xml->updateDoc('newDoc2', 1);
+//$xml->updateDoc('newDoc2', array('keywords'=>'key'), 'item');
+
+$xml->updateItem('title','update title!');
+$xml->updateItem('charset','update windows-1251');
+$xml->updateAttr('group','testDoc');
+$xml->updateItemAttr('url','https://bce.cosmonova.net/service/payu/generate.php');
+$xml->save();
+ */
 
 
+$xml->insert('newDoc2');
+$xml->insertItem('title','insert title!');
+$xml->insertItem('description','insert description!');
 
 
+$xml->insertItem(
+    array(
+        'charset'=>'insert charset UTF-8!',
+        'keywords'=>'insert KEYWORDS!',
+    ));
 
-
-
-
-
-
-
-
-
+$xml->insertAttr('order','120');
+$xml->insertAttr(array(
+        'url'=>'https://bce.cosmonova.net',
+        'group'=>'killers',
+    ));
+    /**/
+$xml->save();
+//var_dump($xml->XMLWrapperInsert->xml);
 
--- source/XMLWrapper.php
+++ source/XMLWrapper.php
@@ -1,208 +1,412 @@
 <?php
 
 
-include 'XMLWrapperDoc.php';
-include 'XMLWrapperItem.php';
+include 'XMLWrapperCreate.php';
+include 'XMLWrapperInsert.php';
+include 'XMLWrapperUpdate.php';
+include 'XMLWrapperDelete.php';
 
-class XMLWrapper
-{
-    public $config = array();
+class XMLWrapper {
+  public $config = array();
 
-    public $_listDocs = array();
-    public $_listItems = array();
+  public $_listDocs = array();
+  public $_listItems = array();
 
-    public $XMLWrapperDoc = null;
-    public $XMLWrapperItem = null;
-
-    public function __construct()
-    {
+  public $XMLWrapperCreate = null;
+  public $XMLWrapperInsert = null;
+  public $XMLWrapperUpdate = null;
+  public $XMLWrapperDelete = null;
 
 
-        $this->config();
-        $this->XMLWrapperDoc = new XMLWrapperDoc();
-        $this->XMLWrapperItem = new XMLWrapperItem();
 
+  public function __construct() {
+    $this->config();
+  }
+
+
+  public function config(array $c = null) {
+    if ($c == null) {
+      $path = dirname(__DIR__) . '/db-xml/';
+      if (!$this->isDir($path)) {
+        $this->error('Не удалось создать директорию!');
+      }
+      $this->config['dbPath'] = $path;
+    } else {
+      if (!empty($c['dbPath'])) {
+        if (!$this->isDir($c['dbPath'])) {
+          $this->error('Не удалось создать директорию!');
+        } else {
+          $this->config = $c;
+        }
+      } else {
+        $this->error('Параметр конфигурации должен быть массивом!');
+      }
+    }
+  }
+
+
+
+  public function doc($doc)
+  {
+    $this->xml($doc);
+    return $this;
+  }
+
+
+  public function toArray($typeBuild = 1) {
+    $iter = 0;
+    $rootItems = array();
+    $xml = $this->xml;
+
+    foreach ($xml->item as $_item) {
+      $_attr = (array) $_item;
+      $_attrShift = array_shift($_attr);
+      if ($typeBuild == 1) {
+        $rootItems[$iter] = $_attr;
+        $rootItems[$iter]['attr'] = $_attrShift;
+      } else {
+        if ($typeBuild == 2) {
+          $rootItems[$iter] = $_attrShift;
+          $rootItems[$iter]['item'] = $_attr;
+        } else {
+          $rootItems[$iter] = array_merge($_attr, $_attrShift);
+        }
+      }
+      $iter++;
     }
 
+    return $rootItems;
+  }
 
-    public function config(array $c = null)
-    {
-        if ($c == null) {
-            $path = dirname(__DIR__) . '/db-xml/';
-            if (!$this->isDir($path)) {
-                $this->error('Не удалось создать директорию!');
+  public function item($id = null, $itemElement = null) {
+    if ($id == null && $this->xml == null) {
+      $this->error('XML resource is empty!');
+    } else {
+      if ($id == null) {
+        return $this->xml;
+      } else {
+        if (is_numeric($id) && $itemElement == null) {
+          foreach ($this->xml->item as $item) {
+            if ((string) $item['id'] == $id) {
+              return $item;
             }
-            $this->config['dbPath'] = $path;
-        } else if (!empty($c['dbPath'])) {
-            if (!$this->isDir($c['dbPath'])) {
-                $this->error('Не удалось создать директорию!');
-            } else
-                $this->config = $c;
-        } else
-            $this->error('Параметр конфигурации должен быть массивом!');
+          }
+        } else {
+          if (is_numeric($id) && $itemElement != null) {
+            foreach ($this->xml->item as $item) {
+              if ((string) $item['id'] == $id) {
+                return (string) $item->$itemElement;
+              }
+            }
+          }
+        }
+      }
     }
+    return null;
+  }
 
-
-    public $_dataXml = null;
-
-    public function doc($doc)
-    {
-        $xml = $this->xml($doc);
-        $this->_dataXml = $xml;
-        return $this;
+  public function attr($attr = null) {
+    $_attrs = (array) $this->xml;
+    if ($attr == null) {
+      return $_attrs['@attributes'];
+    } else {
+      if (isset($_attrs['@attributes'][$attr])) {
+        return $_attrs['@attributes'][$attr];
+      } else {
+        return FALSE;
+      }
     }
+  }
 
-    public function toArray($typeBuild=1)
-    {
-        $iter = 0;
-        $rootItems = array();
-        $xml = $this->_dataXml;
 
-        foreach($xml->item as $_item){
-            $_attr = (array)$_item;
-            $_attrShift = array_shift($_attr);
-            if($typeBuild==1){
-                $rootItems[$iter] = $_attr;
-                $rootItems[$iter]['attr'] = $_attrShift;
-            }else if($typeBuild==2){
-                $rootItems[$iter] = $_attrShift;
-                $rootItems[$iter]['item'] = $_attr;
-            }else{
-                $rootItems[$iter] = array_merge($_attr,$_attrShift);
-            }
-            $iter ++;
-        }
-
-        return $rootItems;
-    }
-
-    public function item($id = null, $itemElement=null)
-    {
-        if ($id == null && $this->_dataXml == null)
-        {
-            $this->error('XML resource is empty!');
-        }
-        else if ($id == null)
-        {
-            return $this->_dataXml;
-        }
-        else if (is_numeric($id) && $itemElement==null)
-        {
-            foreach ($this->_dataXml->item as $item) {
-                if ((string)$item['id'] == $id)
-                    return $item;
-            }
-        }
-        else if (is_numeric($id) && $itemElement!=null)
-        {
-            foreach ($this->_dataXml->item as $item) {
-                if ((string)$item['id'] == $id)
-                    return (string) $item->$itemElement;
-            }
-        }
+  public function __get($item) {
+    if (isset($this->xml[$item])) {
+      return (string) $this->xml[$item];
+    } else {
+      if (isset($this->xml->$item)) {
+        return $this->xml->$item;
+      } else {
         return null;
+      }
+    }
+  }
+
+
+  public function xml($file)
+  {
+    $path = $this->config['dbPath'] . $file . '.xml';
+    if($fileData = file_get_contents($path)){
+      $xml = new SimpleXMLElement($fileData);
+      if($xml){
+        $this->xml = $xml;
+        return $xml;
+      } else
+        return false;
+    } else
+      return false;
+  }
+
+
+  public function listDocs() {
+    $dirRes = dir($this->config['dbPath']);
+    while ($dir = $dirRes->read()) {
+      if ($dir != '.' and $dir != '..') {
+        $this->_listDocs[] = $dir;
+      }
+    }
+    return $this->_listDocs;
+  }
+
+
+  public function listItems($doc) {
+    return $this;
+  }
+
+
+  #          C R E A T E   D O C   M E T H O D S         #
+
+  public function createDoc($failName)
+  {
+    $this->XMLWrapperCreate = new XMLWrapperCreate();
+    $this->XMLWrapperCreate->fileName = $failName;
+    $this->saveType = 'create';
+    return $this;
+  }
+
+
+  /**
+   * @param array $items
+   * @return $this
+   */
+  public function addItems(array $items)
+  {
+    if($this->XMLWrapperCreate==null) $this->error();
+
+    $this->XMLWrapperCreate->addItems($items);
+    return $this;
+  }
+
+
+  /**
+   * @param array $attrs
+   * @return $this
+   */
+  public function addAttrs(array $attrs)
+  {
+    if($this->XMLWrapperCreate==null) $this->error();
+
+    $this->XMLWrapperCreate->addAttrs($attrs);
+    return $this;
+  }
+
+  /**
+   * @param $itemAttr
+   * @return $this
+   */
+  public function addItemsAttrs($itemAttr)
+  {
+    if($this->XMLWrapperCreate==null) $this->error();
+
+    $this->XMLWrapperCreate->addItemsAttrs($itemAttr);
+    return $this;
+  }
+
+  public $saveType = null;
+  public $xml = null;
+
+  /**
+   * @return bool
+   */
+  public function save()
+  {
+    $save = false;
+
+    if($this->saveType == 'create'){
+      # create
+      if($this->XMLWrapperCreate==null) $this->error();
+      $save = $this->XMLWrapperCreate->save();
+
+    }else if($this->saveType == 'select'){
+      # select
+
+    }else if($this->saveType == 'update'){
+      # update
+      if($this->XMLWrapperUpdate==null) $this->error();
+      $save = $this->XMLWrapperUpdate->save();
+
+    }else if($this->saveType == 'insert'){
+      # insert
+      if($this->XMLWrapperInsert==null) $this->error();
+      $save = $this->XMLWrapperInsert->save();
+
+    }else if($this->saveType == 'delete'){
+      # delete
+
     }
 
-    public function attr($attr=null)
+    if($save){
+
+      $this->xml = $save['data'];
+      $saveFile = $this->config['dbPath'].$save['fileName'].".xml";
+
+      if(file_put_contents($saveFile,$save['data']->asXML())){
+        $this->saveType = null;
+        return true;
+      }else{
+        return false;
+      }
+    } else
+      return false;
+
+  }
+
+
+  #          U P D A T E   D O C   M E T H O D S         #
+
+
+  public function updateDoc($failName, $selectValue, $selectLocation='attr')
+  {
+    if($xml = $this->xml($failName))
     {
-        $_attrs = (array)$this->_dataXml;
-        if($attr==null){
-            return $_attrs['@attributes'];
-        }else if(isset($_attrs['@attributes'][$attr])){
-            return $_attrs['@attributes'][$attr];
-        }else
-            return false;
+      $this->XMLWrapperUpdate = new XMLWrapperUpdate();
+      $this->XMLWrapperUpdate->xml = $xml;
+      $this->XMLWrapperUpdate->fileName = $failName;
+
+      if(is_numeric($selectValue)){
+        $this->XMLWrapperUpdate->selectValue = array('id'=>$selectValue);
+        $this->XMLWrapperUpdate->selectLocation = $selectLocation;
+      }else if(is_array($selectValue)){
+        $this->XMLWrapperUpdate->selectValue = $selectValue;
+        $this->XMLWrapperUpdate->selectLocation = $selectLocation;
+      }
+      $this->saveType = 'update';
+      return $this;
+    }else
+      $this->error();
+  }
+
+  public $updateRows = 0;
+  public function updateItem($item, $value)
+  {
+    if($this->XMLWrapperUpdate==null) $this->error();
+    $result = $this->XMLWrapperUpdate->updateItem($item, $value);
+
+    if($result){
+      $this->updateRows += $result;
+      return $this;
     }
+    $this->error();
+  }
 
 
-    public function __get($item)
+  public function updateAttr($item, $value)
+  {
+    if($this->XMLWrapperUpdate==null) $this->error();
+    $result = $this->XMLWrapperUpdate->updateAttr($item, $value);
+
+    if($result){
+      $this->updateRows += $result;
+      return $this;
+    }
+    $this->error('Документ Не существует атрибута: '.$item);
+  }
+
+
+  public function updateItemAttr($item, $value)
+  {
+    if($this->XMLWrapperUpdate==null) $this->error();
+    $result = $this->XMLWrapperUpdate->updateItemAttr($item, $value);
+
+    if($result){
+      $this->updateRows += $result;
+      return $this;
+    }
+    $this->error('Документ Не существует атрибута: '.$item);
+  }
+
+
+
+  #          I N S E R T   M E T H O D S         #
+  public function insert($failName)
+  {
+    if($xml = $this->xml($failName))
     {
-        if (isset($this->_dataXml[$item]))
-        {
-            return (string) $this->_dataXml[$item];
-        }
-        else if(isset($this->_dataXml->$item))
-        {
-            return $this->_dataXml->$item;
-        }
-        else
-            return null;
+      $this->XMLWrapperInsert = new XMLWrapperInsert();
+      $this->XMLWrapperInsert->xml = $xml;
+      $this->XMLWrapperInsert->fileName = $failName;
+
+      $this->saveType = 'insert';
+      return $this;
+    }else
+      $this->error();
+  }
+
+  /** @var int $count  */
+  public $count = 0;
+
+  public function insertItem($item, $value=null)
+  {
+    $result = false;
+    if(is_string($item)){
+      if($this->XMLWrapperInsert==null) $this->error();
+      $result = $this->XMLWrapperInsert->item($item, $value);
+    } else if(is_array($item)){
+      if($this->XMLWrapperInsert==null) $this->error();
+      $result = $this->XMLWrapperInsert->item($item,false);
+    }
+    if($result){
+      $this->count += $result;
+      return $this;
+    }
+  }
+
+
+  public function insertAttr($item, $value=null)
+  {
+    $result = false;
+    if(is_string($item)){
+      if($this->XMLWrapperInsert==null) $this->error();
+      $result = $this->XMLWrapperInsert->attr($item, $value);
+    } else if(is_array($item)){
+      if($this->XMLWrapperInsert==null) $this->error();
+      $result = $this->XMLWrapperInsert->attr($item,false);
     }
 
-    public function xml($file)
-    {
-        $path = $this->config['dbPath'] . '/' . $file . '.xml';
-        $content = new SimpleXMLElement(file_get_contents($path));
-        return $content;
+    if($result){
+      $this->count += $result;
+      return $this;
     }
+  }
+
+  #          D E L E T E   M E T H O D S         #
 
 
-    public function listDocs()
-    {
-        $dirRes = dir($this->config['dbPath']);
-        while ($dir = $dirRes->read()) {
-            if ($dir != '.' and $dir != '..') {
-                $this->_listDocs[] = $dir;
-            }
-        }
-        return $this->_listDocs;
+
+
+
+  #          S Y S T E M   M E T H O D S         #
+
+
+  /**
+   * [isDir description]
+   * @param  [type]  $path [description]
+   * @return boolean       [description]
+   */
+  public function isDir($path) {
+    if (!is_dir($path)) {
+      if (!mkdir($path, 0777, TRUE)) {
+        return FALSE;
+      } else {
+        return TRUE;
+      }
+    } else {
+      return TRUE;
     }
+  }
 
-
-    public function listItems($doc)
-    {
-        return $this;
-    }
-
-
-    public function createDoc()
-    {
-
-    }
-
-
-    public function createItem()
-    {
-
-    }
-
-
-    public function getDoc()
-    {
-
-    }
-
-
-    public function getItem()
-    {
-
-    }
-
-
-
-    #          S Y S T E M   M E T H O D S         #
-
-
-    /**
-     * [isDir description]
-     * @param  [type]  $path [description]
-     * @return boolean       [description]
-     */
-    public function isDir($path)
-    {
-        if (!is_dir($path)) {
-            if (!mkdir($path, 0777, true))
-                return false;
-            else
-                return true;
-        } else
-            return true;
-    }
-
-    public function error($text = '')
-    {
-        die('ERROR: ' . $text);
-    }
+  public function error($text = '') {
+    die('ERROR: ' . $text);
+  }
 }
 
 
